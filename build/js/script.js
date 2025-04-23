ab54725dcfb8e78ab3d860f4dc1d6d80555891cd
tonigs <tonigsanjose@gmail.com>
2025-04-23 09:14:18 +0000

    Update lib

--- build/js/script.js
+++ build/js/script.js
@@ -5,6 +5,7 @@ $(function () {
   let meetMonsters = document.querySelector('#meet')
   let monsterScroll = document.querySelectorAll('#monster-group .monster')
   let navHeight = nav.scrollHeight
+  //TODO: #2 Update the names to make them cleaner
 
   monsterScroll.forEach(
     (item) => (item.style.animationDelay = `${Math.random() * 1 + 0.4}s`)
